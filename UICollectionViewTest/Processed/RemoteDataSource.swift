2fe0c45ba47b904466f9d97a14cdc40688d4db7f
mysafiqq <muhammad.syafiq@geniebook.com>
2024-11-14 13:34:22 +0700

    RDS: Add posts

--- UICollectionViewTest/Processed/RemoteDataSource.swift
+++ UICollectionViewTest/Processed/RemoteDataSource.swift
@@ -10,9 +10,27 @@ import RxSwift
 import CodableWrappers
 
 protocol GenieClassRemoteDataSource {
+    func getPosts() -> Single<[Domain.PostEntity]>
 }
 
 extension DataSource.Remote.GeniebookJsonAPI: GenieClassRemoteDataSource {
+    func getPosts() -> Single<[Domain.PostEntity]> {
+        let endpoint = "posts"
+
+        let param: [String: Any?] = [
+            :
+        ]
+
+        return jsonRequestService
+            .get(
+                to: endpoint,
+                param: param.compactMapValues {
+                    $0
+                },
+                header: [:],
+                type: [Domain.PostEntity].self
+            )
+    }
 }
 
 private extension ApiDataResponse {
